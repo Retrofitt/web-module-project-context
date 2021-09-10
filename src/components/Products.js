4a17385e1471b805d3afc70bb5b0fdd82152d203
Retrofitt <rafaelmendozajr94@gmail.com>
2021-09-09 20:17:53 -0700

    finished

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,14 +3,21 @@ import React from 'react';
 // Components
 import Product from './Product';
 
+//Context
+import { useContext } from 'react';
+import ProductContext from '../contexts/ProductContext';
+
 const Products = props => {
+
+	const { products, addItem } = useContext(ProductContext)
+
 	return (
 		<div className="products-container">
-			{props.products.map(product => (
+			{products.map(product => (
 				<Product
 					key={product.id}
 					product={product}
-					addItem={props.addItem}
+					addItem={addItem}
 				/>
 			))}
 		</div>
